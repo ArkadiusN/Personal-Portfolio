efafdf0014980b4b430021637aae34430535240f
ArkadiusN <arek.nowacki4@gmail.com>
2023-12-04 21:09:54 +0000

    Final stage update 5

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,8 @@
 var root = document.querySelector(":root");
-var toggle = document.querySelector(".switch");
-var checkbox = document.querySelector(".switch__checkbox");
+var toggle = document.querySelector(".side-switch");
+var checkbox = document.querySelector(".side-switch__checkbox");
+var toggle2 = document.querySelector(".switch");
+var checkbox2 = document.querySelector(".switch__checkbox");
 
 // Instead of changing the root variables
 // create a $vars in stylesheet and then another one
@@ -9,6 +11,22 @@ var checkbox = document.querySelector(".switch__checkbox");
 // Make transitions on this change once
 toggle.addEventListener("change", () => {
   if (checkbox.checked) {
+    console.log("Checked");
+    root.style.setProperty("--dark-purple", "#362222");
+    root.style.setProperty("--navy", "#171010");
+    root.style.setProperty("--orange", "#423F3E");
+    root.style.setProperty("--white", "#D7D7D7");
+  } else {
+    console.log("Unchecked");
+    root.style.setProperty("--dark-purple", "#222831");
+    root.style.setProperty("--navy", "#2d4059");
+    root.style.setProperty("--orange", " #ff5722");
+    root.style.setProperty("--white", "#ffffff");
+  }
+});
+
+toggle2.addEventListener("change", () => {
+  if (checkbox2.checked) {
     console.log("Checked");
     root.style.setProperty("--dark-purple", "#362222");
     root.style.setProperty("--navy", "#171010");
--- js/transition-change.js
+++ js/transition-change.js
@@ -1,7 +1,9 @@
-var toggle = document.querySelector(".switch");
-var checkbox = document.querySelector(".switch__checkbox");
+var toggle = document.querySelector(".side-switch");
+var checkbox = document.querySelector(".side-switch__checkbox");
 var button1 = document.querySelector(".btn-one");
 var button2 = document.querySelector(".btn-two");
+var toggle2 = document.querySelector(".switch");
+var checkbox2 = document.querySelector(".switch__checkbox");
 
 // var aTags = document.querySelectorAll(".none");
 var aTag1 = document.querySelector(".none1");
@@ -17,12 +19,6 @@ toggle.addEventListener("change", () => {
     githubIcon.classList.toggle("darkModeReading");
     aTag1.classList.toggle("darkModeReading");
     aTag2.classList.toggle("darkModeReading");
-
-    // if (aTags.length > 0) {
-    //   for (var i = 0; i < aTags.length; i++) {
-    //     aTags[i].classList.toggle("darkModeReading");
-    //   }
-    // }
   } else {
     console.log("Unchecked");
     button1.classList.remove("darkModeReading");
@@ -30,10 +26,24 @@ toggle.addEventListener("change", () => {
     githubIcon.classList.remove("darkModeReading");
     aTag1.classList.remove("darkModeReading");
     aTag2.classList.remove("darkModeReading");
-    // if (aTags.length > 0) {
-    //   for (var i = 0; i < aTags.length; i++) {
-    //     aTags[i].classList.remove("darkModeReading");
-    //   }
-    // }
   }
 });
+
+toggle2.addEventListener("change", () => {
+  if (checkbox2.checked) {
+    console.log("Checked");
+    button1.classList.toggle("darkModeReading");
+    button2.classList.toggle("darkModeReading");
+    githubIcon.classList.toggle("darkModeReading");
+    aTag1.classList.toggle("darkModeReading");
+    aTag2.classList.toggle("darkModeReading");
+  } else {
+    console.log("Unchecked");
+    button1.classList.remove("darkModeReading");
+    button2.classList.remove("darkModeReading");
+    githubIcon.classList.remove("darkModeReading");
+    aTag1.classList.remove("darkModeReading");
+    aTag2.classList.remove("darkModeReading");
+  }
+});
+
